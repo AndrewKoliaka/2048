6b0b52e6403009032ed2bdebd2e6b73ac1474139
AndrewKoliaka <andrew.koliaka@gmail.com>
2017-06-05 12:36:29 +0300

    action types created

---
+++ src/store/actions.js
@@ -0,0 +1,15 @@
+export const gameActionTypes = {
+    START: 'START',
+    GAME_OVER: 'GAME_OVER',
+    WIN: 'WIN'
+};
+
+export const boardActionTypes = {
+    NEW_TILE: 'NEW_TILE',
+    MOVE_TILES: 'MOVE_TILES',
+    MERGE_TILES: 'MERGE_TILES'
+};
+
+export const scoreActionTypes = {
+    RESET: 'RESET'
+};
